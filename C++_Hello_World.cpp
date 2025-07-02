67242a6410c7d5253531b0e9d9c7414e3c54204d
Harsh1118 <harsh.dubey2023@ssipmt.com>
2025-07-02 16:45:58 +0530

    Create C++_Hello_World.cpp

---
+++ C++_Hello_World.cpp
@@ -0,0 +1,24 @@
+/*
+Let's begin your journey towards coding with the very first question of the coding world. Your task is to write code that prints Hello World.
+
+Example 1:
+
+Input:
+No Input
+Output:
+Hello World
+Expected Time Complexity: O(1)
+Expected Auxiliary Space: O(1)
+*/
+#include<iostream>
+using namespace std;
+class Solution {
+  public:
+    void helloWorld() {
+
+        // Type below cout<<"Hello World";
+        // Radhe-Radhe
+        cout<<"Hello World" ;
+        // cout will output to console
+    }
+};

fd1497f7a50b0686fdd36f3e1508d050c5c32b6e
Harsh1118 <harsh.dubey2023@ssipmt.com>
2025-08-27 17:49:46 +0530

    Create Circle_of_strings.cpp

---
+++ Circle_of_strings.cpp
@@ -0,0 +1,82 @@
+
+//    This is a Hard Level Interview Question
+
+// Given an array arr of lowercase strings, determine if the strings can be chained together to form a circle.
+// A string X can be chained together with another string Y if the last character of X is the same as the first character of Y. If every string of the array can be chained with exactly two strings of the array(one with the first character and the second with the last character of the string), it will form a circle.
+
+// For example, for the array arr[] = {"for", "geek", "rig", "kaf"} the answer will be Yes as the given strings can be chained as "for", "rig", "geek" and "kaf"
+
+// Examples
+
+// Input: arr[] = ["abc", "bcd", "cdf"]
+// Output: 0
+// Explaination: These strings can't form a circle because no string has 'd'at the starting index.
+// Input: arr[] = ["ab" , "bc", "cd", "da"]
+// Output: 1
+// Explaination: These strings can form a circle of strings.
+// Expected Time Complexity: O(n)
+// Expected Auxiliary Space: O(n)
+
+// Constraints:
+// 1 ≤ length of strings ≤ 20
+
+#include <iostream>
+#include <vector>
+#include <algorithm>
+using namespace std;
+
+class Solution
+{
+public:
+    void DFS(int node, vector<int> adj[], vector<bool> &visited)
+    {
+        visited[node] = 1;
+
+        for (int j = 0; j < adj[node].size(); j++)
+        {
+            if (!visited[adj[node][j]])
+            {
+                DFS(adj[node][j], adj, visited);
+            }
+        }
+    }
+
+    int isCircle(vector<string> &arr)
+    {
+        // code here
+        int N = arr.size();
+
+        vector<int> adj[26];
+        vector<int> Outdeg(26, 0);
+        vector<int> Indeg(26, 0);
+        for (int i = 0; i < N; i++)
+        {
+            string temp = arr[i];
+            int u = temp[0] - 'a';
+            int v = temp[temp.size() - 1] - 'a';
+            adj[u].push_back(v);
+            Outdeg[u]++;
+            Indeg[v]++;
+        }
+
+        for (int i = 0; i < 26; i++)
+        {
+            if (Indeg[i] != Outdeg[i])
+            {
+                return 0;
+            }
+        }
+        vector<bool> visited(26, 0);
+        int node = arr[0][0] - 'a';
+        DFS(node, adj, visited);
+
+        for (int i = 0; i < 26; i++)
+        {
+            if (Indeg[i] && !visited[i])
+            {
+                return 0;
+            }
+        }
+        return 1;
+    }
+};
